651fcfa1dedc5f237bb0945a243c50cb76e1062c
renae-stl <74110191+renae-stl@users.noreply.github.com>
2021-08-31 15:15:47 +0100

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+    backend "azurerm" {
+        resource_group_name  = "Team2ResourceGroup2"
+        storage_account_name = "makersteam2storage"
+        container_name       = "makersteam2container"
+        key                  = "terraform.tfstate"
+    }
+
+}
