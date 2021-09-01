908a573f4744162084f856a39b3cc589b15d692c
usna.hakimi95@gmail.com <usnahakimi@MA374.local>
2021-09-01 13:45:59 +0100

    main.tf changed

--- main.tf
+++ main.tf
@@ -1,15 +1,16 @@
-provider "azurerm" {
-  version = "=2.46.0"
-  features {}
-}
-
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "Team2ResourceGroup2"
-    storage_account_name = "makersteam2storage"
-    container_name       = "makersteam2container"
-    key                  = "terraform.tfstate"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
   }
+    backend "azurerm" {
+        resource_group_name  = "Team2ResourceGroup2"
+        storage_account_name = "makersteam2storage"
+        container_name       = "makersteam2container"
+        key                  = "terraform.tfstate"
+    }
 }
 resource "azurerm_resource_group" "rg-hello-azure" {
   name     = "rg-hello-azure"
