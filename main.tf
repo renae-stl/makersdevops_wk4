0aab0780dae50d6990e3968232f22e8721d0bc06
Alex <alexandrachorbe@MA324.local>
2021-09-01 14:34:55 +0100

    test

--- main.tf
+++ main.tf
@@ -8,16 +8,16 @@ terraform {
       version = "=2.46.0"
     }
   }
-    backend "azurerm" {
-      resource_group_name  = "Team2ResourceGroup2"
-      storage_account_name = "makersteam2storage"
-      container_name       = "makersteam2container"
-      key                  = "terraform.tfstate"
-    }
+  backend "azurerm" {
+    resource_group_name  = "Team2ResourceGroup2"
+    storage_account_name = "makersteam2storage"
+    container_name       = "makersteam2container"
+    key                  = "terraform.tfstate"
+  }
 }
 resource "azurerm_resource_group" "rg-hello-azure" {
   name     = "rg-hello-azure"
   location = "westeurope"
-}    
+}
 
 
