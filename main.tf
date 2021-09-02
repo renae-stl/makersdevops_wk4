90e02c7bcfe070c165687999b5033b3ffee920cb
Yusuf <yakhaliq01@gmail.com>
2021-09-02 11:49:35 +0100

    db and fmt

--- main.tf
+++ main.tf
@@ -48,7 +48,31 @@ resource "azurerm_app_service" "main" {
   site_config {
     linux_fx_version = "NODE|10.14"
   }
+}
 
+resource "azurerm_postgresql_server" "example" {
+  name                = "team2-postgresql-server"
+  location            = "westeurope"
+  resource_group_name = "Team2ResourceGroup2"
+
+  sku_name = "B_Gen5_2"
+
+  storage_mb            = 5120
+  backup_retention_days = 7
+  auto_grow_enabled     = false
+
+  administrator_login          = "safeadmin"
+  administrator_login_password = "safepassword123!"
+  version                      = "9.5"
+  ssl_enforcement_enabled      = false
+}
+
+resource "azurerm_postgresql_database" "example" {
+  name                = "team2_db"
+  resource_group_name = "Team2ResourceGroup2"
+  server_name         = azurerm_postgresql_server.example.name
+  charset             = "UTF8"
+  collation           = "English_United States.1252"
 }
 
 
